97e8f3166e9ab5341502f188f194f40571f28284
jacekk <jakon8@gmail.com>
2021-10-12 18:55:15 +0200

    first revision

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const figlet = require("figlet");
+const weirdFonts = require("weird-fonts");
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send(`<h1>${weirdFonts.circle("Hello World!")}</h1>`)
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000...")
+})
+
